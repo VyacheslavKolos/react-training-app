b988029f8218d44e9cc926afc5d4fe823a609799
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-06-24 21:10:25 +0300

    before details component

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import {ExerciseOptions, fetchData} from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+const ExerciseDetail = () => {
+    return (
+        <Box>
+            <Detail/>
+            <ExerciseVideos/>
+            <SimilarExercises/>
+        </Box>
+    );
+};
+
+export default ExerciseDetail;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const ExerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    return await response.json();
+};
